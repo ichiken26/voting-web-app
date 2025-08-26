2bd346cbc035224a4c40879f44df2ec3edcc5722
ichiken26 <62ichiken@gmail.com>
2025-08-26 23:39:27 +0900

    feat: ベースURLを変更し、GitHubPagesにデモをデプロイしやすいように

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Unfonts from 'unplugin-fonts/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/voting-web-app/',
+  base: '/demo_voting-web-app/',
   plugins: [
     vue(),
     vueDevTools(),
